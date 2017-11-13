f782e9ebd339021b053ee3541cb0c3144e3b45b6
fagui <fagui@Maria>
2017-11-13 12:25:39 +0100

    Creo la clase Rey con sus correspondientes atributos

---
+++ Eguizabal_Alamanac_Fernando_PROG_Tarea03/src/tarea03/Rey.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ *
+ * @author Fernando Eguizabal Alamañac
+ */
+public class Rey {
+    //Creamos los atributos del rey con el tipo de los enum y clases que hemos creado
+    public Color color;
+    public Posicion posicion;
+}
