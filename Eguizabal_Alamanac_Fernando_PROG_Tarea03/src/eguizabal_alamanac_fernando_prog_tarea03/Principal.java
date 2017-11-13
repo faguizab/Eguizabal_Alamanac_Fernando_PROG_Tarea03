7f72b0e983a643f80ba20a6855d9c78075699caa
fagui <fagui@Maria>
2017-11-13 15:19:51 +0100

    creo el metodo mueve para comprobar y establecer la posicion del rey en funcion de la dirección pasada por parámetros

---
+++ Eguizabal_Alamanac_Fernando_PROG_Tarea03/src/eguizabal_alamanac_fernando_prog_tarea03/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eguizabal_alamanac_fernando_prog_tarea03;
+
+/**
+ *
+ * @author fagui
+ */
+public class Principal {
+    //Creamos el método Main
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
