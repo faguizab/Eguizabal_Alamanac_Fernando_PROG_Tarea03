a8975b2c33d3e5a71ccd25d22d93c854249215cd
fagui <fagui@Maria>
2017-11-13 12:35:15 +0100

    Creo el constructor para la clase rey y en funcion de el color pasado inicializamos la fila y columna al correspondiente

--- Eguizabal_Alamanac_Fernando_PROG_Tarea03/src/tarea03/Rey.java
+++ Eguizabal_Alamanac_Fernando_PROG_Tarea03/src/tarea03/Rey.java
@@ -13,4 +13,16 @@ public class Rey {
     //Creamos los atributos del rey con el tipo de los enum y clases que hemos creado
     public Color color;
     public Posicion posicion;
+    
+    //creamos el constructor en funcion del color que nos pasan
+    public Rey (String color){
+        //según sea el color psado por parametros creamos la posicion
+        if(color.equalsIgnoreCase("BLANCO")){
+            this.posicion.setFila(1);
+            this.posicion.setColumna('e');
+        }else{
+            this.posicion.setFila(8);
+            this.posicion.setColumna('e');
+        }
+    }
 }
